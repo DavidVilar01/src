6e487c764fa1d41f99d267a0098df8a0cde7d552
DavidVilar01 <david.vilar01@estudiant.upf.edu>
2023-10-30 18:19:02 +0100

    LAb2

--- League.java
+++ League.java
@@ -8,21 +8,21 @@ public class League {
     }
 
     public void generateMatches() {
-        // Iterate over all pairs of teams in a double loop to create matches
+        
         for (int i = 0; i < teams.size(); i++) {
             for (int j = i + 1; j < teams.size(); j++) {
                 Team homeTeam = teams.get(i);
                 Team awayTeam = teams.get(j);
 
-                // Create home and away matches
+                
                 Match homeMatch = new Match(homeTeam, awayTeam);
                 Match awayMatch = new Match(awayTeam, homeTeam);
 
-                // Simulate the matches
+                
                 homeMatch.simulateMatch();
                 awayMatch.simulateMatch();
 
-                // Update statistics for participating teams
+                
                 homeTeam.updateStats(homeMatch);
                 awayTeam.updateStats(awayMatch);
             }
@@ -32,7 +32,7 @@ public class League {
 
     
     public void simulateMatches() {
-        // Simulate all matches in the league
+        
         for (Team team : teams) {
             team.simulatMatches();
         }
